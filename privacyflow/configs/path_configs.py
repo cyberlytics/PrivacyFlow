bc3d63372f6d462e327d2879fc9766f882bfb800
patsab <sabau.pa@gmail.com>
2023-08-25 17:56:42 +0200

    Update notebooks

--- privacyflow/configs/path_configs.py
+++ privacyflow/configs/path_configs.py
@@ -1,4 +1,5 @@
 ROOT_PATH = "./privacyflow"
+MODELS_TRAINED_BASE_PATH = f"{ROOT_PATH}/models_trained"
 
 IMG_FOLDER_PATH = f"{ROOT_PATH}/data_faces/img_align_celeba/img_align_celeba"
 IMG_INFO_PATH = f"{ROOT_PATH}/data_faces/list_attr_celeba.csv"
@@ -14,10 +15,18 @@ HEART_DATA_VAL = f"{ROOT_PATH}/data_health/heart_val.csv"
 HEART_DATA_TEST = f"{ROOT_PATH}/data_health/heart_test.csv"
 HEART_DATA_TOTAL = f"{ROOT_PATH}/data_health/heart_total.csv"
 
+
 FACE_MI_DATA = f"{ROOT_PATH}/data_faces/faces_mi.csv"
-FACE_BASE_MODEL = f"{ROOT_PATH}/models_trained/face_base_model.pl"
+
+FACE_CNN_MODEL = f"{ROOT_PATH}/models_trained/face_cnn_model.pl"
 FACE_DENSE_MODEL = f"{ROOT_PATH}/models_trained/face_dense_model.pl"
 FACE_VIT_MODEL = f"{ROOT_PATH}/models_trained/face_vit_model.pl"
+
 FACE_VIT_NP_MODEL = f"{ROOT_PATH}/models_trained/face_vit_np_model.pl"
+FACE_CNN_NP_MODEL = f"{ROOT_PATH}/models_trained/face_cnn_np_model.pl"
+
 FACE_CNN_DPSGD = f"{ROOT_PATH}/models_trained/face_cnn_dpsgd_model.pl"
 FACE_VIT_DPSGD = f"{ROOT_PATH}/models_trained/face_vit_dpsgd_model.pl"
+
+FACE_CNN_NP_DPSGD = f"{ROOT_PATH}/models_trained/face_cnn_np_dpsgd_model.pl"
+FACE_VIT_NP_DPSGD = f"{ROOT_PATH}/models_trained/face_vit_np_dpsgd_model.pl"
